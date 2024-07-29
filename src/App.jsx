a94a64f40cf72a9701c5630b519add695d5ce26b
Tusharprabhu <tusharprabhu2004@gmail.com>
2024-07-29 19:59:33 +0530

    chore: Add react-icons package to dependencies and update package-lock.json and package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
-
+import {FalinkedIn} from 'react-icons/fa';
 function App() {
-return(
-  <>
-  <h2>React App</h2>
-  </>
-)
+  return (
+    <>
+      <div className=" bg-red-900 py-6 nb-20 flex items-center justify-between">
+        <div className="flex flex-shrink-0 items-center">
+          <h1 className="text-white text-2xl">Tushar</h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
