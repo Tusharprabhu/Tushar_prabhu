3b26939dca5b846c5662e8b1f9711494adad6dd6
Tusharprabhu <tusharprabhu2004@gmail.com>
2024-07-31 21:04:53 +0530

    chore: Update image loading and import paths in Experience and Project components

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { PROJECTS } from "../constants";
-
+import { motion } from "framer-motion";
 const Project = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
       <h1 className="my-20 text-center text-4xl">Projects</h1>
       <div>
         {PROJECTS.map((project, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:w-1/4">
+          <div key={index} className="m-16 flex flex-wrap lg:justify-center">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              whileHover={{ scale: 1.1 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.7 }}
+              className="w-full lg:w-1/4"
+            >
               <img
                 src={project.image}
                 width={150}
@@ -16,9 +22,16 @@ const Project = () => {
                 alt={project.title}
                 className="mb-6 rounded"
               />
-            </div>
-            <div className="w-full max-x-xl lg:w-3/4">
-              <h6 className="mb-2 font-semibold text-pink-600">{project.title}</h6>
+            </motion.div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 0.7 }}
+              className="w-full max-x-xl lg:w-3/4"
+            >
+              <h6 className="mb-2 font-semibold text-pink-600">
+                {project.title}
+              </h6>
               <p className="mb-4 text-neutral-400">{project.description}</p>
               {project.technologies.map((tech, index) => (
                 <span
@@ -28,7 +41,7 @@ const Project = () => {
                   {tech}
                 </span>
               ))}
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>
--- src/component/hero.jsx
+++ src/component/hero.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/kevinRushProfile.png";
-import { motion } from "framer-motion"
-const a=-100;
-const c=100;
-const container =(delay,x) => ({
-hidden:{x: x, opacity: 0},
-visible:{
-  x:0,
-  opacity:1,
-  transition:{duration:0.5,delay:delay}
-},
+import { motion } from "framer-motion";
+const a = -100;
+const c = 100;
+const container = (delay, x) => ({
+  hidden: { x: x, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
 });
 const Hero = () => {
   return (
@@ -21,29 +21,45 @@ const Hero = () => {
             className=" flex flex-col items-center 
           lg:items-start h-1/2 justify-center"
           >
-            <motion.h1 variants={container(0.2,a)} initial="hidden" animate="visible"
+            <motion.h1
+              whileHover={{ scale: 1.06 }}
+              whileTap={{ scale: 1.1 }}
+              drag={true}
+              dragConstraints={{ left: -80, right:200, top: -100, bottom: 3900 }}
+              variants={container(0.4, a)}
+              initial="hidden"
+              animate="visible"
+              
               className="pt-64 pb-16 lg:py-24 text-6xl font-thin tracking-tight 
             lg:text-8xl"
             >
               Tushar<br></br>Prabhu
             </motion.h1>
-            <motion.div variants={container(0.4,a)} initial="hidden" animate="visible">
-            <span
-              className="bg-gradient-to-r from-pink-300 
+            <motion.div
+              variants={container(0.4, a)}
+              initial="hidden"
+              animate="visible"
+            >
+              <span
+                className="bg-gradient-to-r from-pink-300 
             via-slate-500  to-purple-500 bg-clip-text text-3xl 
             tracking-tight text-transparent font-light "
-            >
-              Electronics and Communication Engineer
-            </span>
-            <p>{HERO_CONTENT}
-
-            </p>
+              >
+                Electronics and Communication Engineer
+              </span>
+              <p>{HERO_CONTENT}</p>
             </motion.div>
           </div>
         </div>
         <div className=" lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <motion.img variants={container(0.6,c)} initial="hidden" animate="visible" src={profilePic} alt="Tushar Prabhu" />
+            <motion.img
+              variants={container(0.6, c)}
+              initial="hidden"
+              animate="visible"
+              src={profilePic}
+              alt="Tushar Prabhu"
+            />
           </div>
         </div>
       </div>
