34ef2be84152aca05c77a5efe90ce200dbed6209
Tusharprabhu <tusharprabhu2004@gmail.com>
2024-08-11 01:11:37 +0530

    feat: Enable Experience section in App component

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
           <About />
           <Technologies />
           <Projects />
-          {/* <Experience /> */}
+          <Experience />
           <Contact />
         </div>
       </div>
