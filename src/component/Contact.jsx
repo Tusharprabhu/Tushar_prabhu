0b91ae1f1163d96fdbf7b72b98cf930f932c9fe1
Tusharprabhuu <tusharprabhu2004@gmail.com>
2025-08-15 23:27:21 +0530

    did some shit

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
   }, []);
 
   return (
-    <div ref={contactRef} className="contact-container border-b border-neutral-900 pb-4 h-screen">
+    <div ref={contactRef} className="contact-container border-b border-neutral-900 pb-4 h-screen flex flex-col items-center justify-center">
       <h1 className="mt-10 text-center text-4xl font-bemirs">Contact me</h1>
       <section className="bg-black ">
         <div className="container px-6 py-12 mx-auto">
